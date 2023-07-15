ab9080a698698cba6ec6c5cddffc38e2b091e3eb
Cristin O'Connor <her@cristin.io>
2023-07-14 18:59:49 -0600

    Refining

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState, useCallback } from 'react'
+import InputGroup from 'react-bootstrap/InputGroup';
 import Image from 'react-bootstrap/Image';
 import Stack from 'react-bootstrap/Stack';
 import Spinner from 'react-bootstrap/Spinner';
@@ -8,6 +9,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
 
 import fauxData from './fauxData';
 
@@ -15,26 +17,22 @@ const Video = ({id}) => {
   return <iframe className="p-0" key={id} title={id} width="100%" height="300px" src={`https://www.youtube.com/embed/${id}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 }
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(fauxData);
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
   const [activeItem, setActiveItem] = useState(null);
-  let URL = "https://www.googleapis.com/youtube/v3/search"
   let options = {
-      key: process.env.REACT_APP_GOOGLE_API_KEY,
-      q: query,
-      part: 'snippet'
-    }
+    key: process.env.REACT_APP_GOOGLE_API_KEY,
+    q: query,
+    part: 'snippet'
+  }
+  let URL = "https://www.googleapis.com/youtube/v3/search?" + Object.keys(options).map((k) => k + '=' + encodeURIComponent(options[k])).join('&');
 
-  URL += '?' + Object.keys(options).map((k) => k + '=' + encodeURIComponent(options[k])).join('&');
   let loadVideoResults = useCallback(
-
     async function () {
+      setLoading(true);
 
-
-    setLoading(true);
-
-    await fetch(URL)
+      await fetch(URL)
       .then(res => res.json())
       .then(data => {
         setData(data);
@@ -44,57 +42,89 @@ function App() {
         setLoading(false);
         console.log(err)
       })
-  }, [URL])
+    }, [URL])
 
-  useEffect(() => {
-    if (data === null) {
-      loadVideoResults();
-    }
-  }, [data, loadVideoResults])
+    useEffect(() => {
+      if (data === null) {
+        loadVideoResults();
+      }
+    }, [data, loadVideoResults])
 
-return (
-  <Container>
-    <Row className="App">
-      <Col md={6}>
-            <Form onSubmit={(e) => {
-                e.preventDefault();
-                setQuery(e.target.value);
-                loadVideoResults();
-              }}>
-              <Form.Group className="mb-3">
-                <Form.Label>Search YouTube:</Form.Label>
-                <Form.Control name="query" type="text" placeholder="Search YouTube" value={query} onChange={(e) => {
-                  setQuery(e.target.value);
-                }} />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-            <Video id={activeItem} />
+    return (
+      <Container>
+        <Row className="App">
+          <Col md={12}>
+            <Navbar expand="md">
+              <Container>
+                <Navbar.Brand href="/">Search YouTube</Navbar.Brand>
+              </Container>
+            </Navbar>
           </Col>
           <Col md={6}>
-
-        <Stack gap={4}>
-          {loading ? <Row><Col><Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner></Col></Row> : data?.items?.map((item) => {
-          console.log("item", item);
-          return <Card key={item.snippet.title}><Card.Header><h4>{item.snippet.title} &bull; {item.snippet.channelTitle}</h4></Card.Header><Card.Body>
-            <Image src={item.snippet.thumbnails.medium.url} thumbnail onClick={(e) => {
+            <Container>
+            <Form onSubmit={(e) => {
               e.preventDefault();
-              setActiveItem(item.id.videoId);
-            }}/>
-          </Card.Body></Card>
-          })}
-        </Stack>
+              setQuery(e.target.value);
+              loadVideoResults();
+            }}>
+              <InputGroup className="mb-3">
+                <Form.Control
+                  name="query"
+                  type="text"
+                  placeholder="Search YouTube"
+                  aria-describedby="search-btn"
+                  value={query}
+                  onChange={(e) => {
+                    setQuery(e.target.value);
+                  }}
+                />
 
+                <Button
+                  variant="primary"
+                  type="submit"
+                  md={4}
+                  lg={2}
+                  id="search-btn"
+                >
+                  Search
+                </Button>
+              </InputGroup>
+            </Form>
+          </Container>
+          <Container>
+            <Video id={activeItem} />
+          </Container>
+        </Col>
+        <Col md={6}>
+          <Stack gap={4}>
+            {loading ? (
+              <Row>
+                <Col>
+                  <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span></Spinner>
+                </Col>
+              </Row>
+            ) : data?.items?.map((item) => {
+              return <Card key={item.snippet.title}>
+                <Card.Header>
+                  <h4>{item.snippet.title} &bull; {item.snippet.channelTitle}</h4>
+                </Card.Header>
+                <Card.Body>
+                  <Image
+                    src={item.snippet.thumbnails.medium.url}
+                    thumbnail
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setActiveItem(item.id.videoId);
+                    }}
+                  />
+              </Card.Body>
+              </Card>})}
+            </Stack>
+          </Col>
+        </Row>
+      </Container>
+      );
+    }
 
-
-      </Col>
-    </Row>
-  </Container>
-  );
-}
-
-export default App;
+    export default App;
